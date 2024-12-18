faba143ac65a24dede417c6c79e4146993599600
Keo Kay <keokay888@gmail.com>
2024-12-18 14:56:05 +0700

    constructors

--- src/classdemo/Main.java
+++ src/classdemo/Main.java
@@ -4,15 +4,19 @@ package classdemo;
 
 public class Main {
     public static void main(String[] args) {
-        // Create obj from student class
-        Student student2 = new Student();
-            student2.username="bona";
 
-        Student student1 = new Student();
-            student1.username="james";
-            student1.gender = "male";
-            student1.score = 100;
-            student1.id="ID0001";
+        Student student3 = new Student();
+//        student3.input();
+//        student3.showInfo();
+        // Create obj from student class
+        Student student2 = new Student("ID0002","Bona liza","female",60); // default constructor
+//            student2.username="bona";
+
+        Student student1 = new Student("ID0001","Kok Bona","male",90);
+//            student1.username="james";
+//            student1.gender = "male";
+//            student1.score = 100;
+//            student1.id="ID0001";
 
         student1.showInfo(); // show info of obj student 1
         student2.showInfo();
--- src/classdemo/Student.java
+++ src/classdemo/Student.java
@@ -7,8 +7,28 @@ public class Student {
     String gender;
     float score;
 
+    // No parameter constructor
+   /* public Student(){
+        System.out.println("This is the default constructor ");
+        id="<Unknown>";
+        gender="male";
+        score = 10;
+    }*/
+    public Student(){}
+    // Parameterized Constructor
+    public  Student(String id,
+                    String username,
+                    String gender,
+                    float score ){
+        // this keyword represent the current object
+        this.id = id;
+        this.username = username;
+        this.gender = gender;
+        this.score = score;
+    }
     // methods
     void showInfo(){
+        System.out.println("------------------------------");
         System.out.println("ID: "+id);
         System.out.println("Name: "+username);
         System.out.println("Gender: "+gender);
